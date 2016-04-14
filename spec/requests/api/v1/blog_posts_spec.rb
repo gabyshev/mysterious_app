7627af168162dca78f828a49d621adca7417ea85
Gabyshev Semyen <gabyshev.sa@gmail.com>
2016-04-15 02:38:46 +0600

    fix blog posts spec

--- spec/requests/api/v1/blog_posts_spec.rb
+++ spec/requests/api/v1/blog_posts_spec.rb
@@ -2,9 +2,25 @@ require 'rails_helper'
 
 RSpec.describe 'Blog Posts API', type: :request do
   let!(:blog_post) { create :blog_post }
+  let(:user) { create :user }
+  let(:params) { { blog_post: { title: 'title', body: 'body', user_id: user } } }
+
+  context '#index' do
+    it 'returns array of blog posts' do
+      get blog_posts_path
+      expect(response.status).to eq 200
+      expect(json).to be_kind_of Array
+    end
+  end
+
+  context '#show' do
+    it 'returns blog post' do
+      get blog_post_path(blog_post)
+      expect(response.status).to eq 200
+    end
+  end
 
   describe 'authorized' do
-    let(:user) { create :user }
     let(:token) do
       sign_in user: user
       json['token']
@@ -12,17 +28,8 @@ RSpec.describe 'Blog Posts API', type: :request do
     let(:headers) { { 'Authorization' => "Bearer #{user.email} #{token}" } }
 
     context '#create' do
-      let(:params) { { blog_post: { title: 'title', body: 'body', user_id: user } } }
       subject(:req) { post blog_posts_path, params, headers }
-
       it { expect { req }.to change { BlogPost.count }.by(1) }
-    end
-
-    context '#show' do
-      it 'returns blog post' do
-        get blog_post_path(blog_post), nil, headers
-        expect(response.status).to eq 200
-      end
     end
 
     context '#update' do
@@ -45,23 +52,25 @@ RSpec.describe 'Blog Posts API', type: :request do
   end
 
   describe 'anonymous' do
-    context 'returns 401 error' do
-      let(:params) { { blog_post: { title: 'foo', body: 'bar' } } }
-      it 'on #create' do
+    context '#create' do
+      it 'returns error' do
         post blog_posts_path, params
-        expect(response.status).to eq 401
-      end
-
-      it 'on #update' do
-        put blog_post_path(blog_post), params
-        expect(response.status).to eq 401
-      end
-
-      it 'on #destroy' do
-        delete blog_post_path(blog_post)
         expect(response.status).to eq 401
       end
     end
 
+    context '#update' do
+      it 'returns error' do
+        put blog_post_path(blog_post), params
+        expect(response.status).to eq 401
+      end
+    end
+
+    context '#destroy' do
+      it 'returns error' do
+        delete blog_post_path(blog_post)
+        expect(response.status).to eq 401
+      end
+    end
   end
 end
