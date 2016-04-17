ffa0d2903cac65e7a81a2fb0231a35b43af44352
Gabyshev Semyen <gabyshev.sa@gmail.com>
2016-04-18 00:41:01 +0600

    add missing associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  has_many :blog_posts
+  has_many :comments
 end
