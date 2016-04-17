9de313cc56d70095f3710d3f71ae961617eab7b3
Gabyshev Semyen <gabyshev.sa@gmail.com>
2016-04-18 00:42:16 +0600

    styling

--- spec/support/request_helper.rb
+++ spec/support/request_helper.rb
@@ -8,7 +8,7 @@ module Requests
   module SessionHelpers
     def sign_in(user: create(:user))
       post user_session_path,
-                 user: { email: user.email, password: user.password }
+           user: { email: user.email, password: user.password }
     end
   end
 end
