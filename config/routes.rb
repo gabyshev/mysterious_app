fefaca62b806ae8aaa249ab73beca4ed217242dd
Gabyshev Semyen <gabyshev.sa@gmail.com>
2016-04-18 01:11:42 +0600

    added api constraint class

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
-  scope module: 'v1', defaults: { format: :json } do
+  def api_version(version, &routes)
+    api_constraint = ApiConstraint.new(version: version)
+    scope(
+      module: "v#{version}",
+      defaults: { format: :json },
+      constraints: api_constraint, &routes
+    )
+  end
+
+  api_version(1) do
     devise_for :users, controllers: {
       sessions:      'v1/users/sessions',
       registrations: 'v1/users/registrations'
