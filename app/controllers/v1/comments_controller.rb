aa1e0ce60201fe3f05dc9f395becb0a39651b0a1
Gabyshev Semyen <gabyshev.sa@gmail.com>
2016-04-18 17:36:34 +0600

    fix comment policy; it shouldnt check create action

--- app/controllers/v1/comments_controller.rb
+++ app/controllers/v1/comments_controller.rb
@@ -5,7 +5,6 @@ class V1::CommentsController < ApplicationController
     blog_post = BlogPost.find(params[:blog_post_id])
     comment = blog_post.comments.new(comments_params)
     comment.user = current_user
-    authorize comment
     comment.save
     respond_with comment, location: blog_post_comments_path(blog_post)
   end
--- app/policies/comment_policy.rb
+++ app/policies/comment_policy.rb
@@ -1,8 +1,4 @@
 class CommentPolicy < ApplicationPolicy
-  def update?
-    @user.admin? || record.user == @user
-  end
-
   def destroy?
     @user.admin? || record.user == @user
   end
