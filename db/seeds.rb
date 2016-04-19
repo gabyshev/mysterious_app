cd9aaf224bf4d08ccd539f9eadcbebee066fdb28
Gabyshev Semyen <gabyshev.sa@gmail.com>
2016-04-19 11:34:38 +0600

    added notification messages to seeds and admin user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,9 @@
+puts 'Creating users..'
+User.create(email: 'admin@test.com', password: '123123123', role: 'admin')
 user = User.create(email: 'test@test.com', password: '123123123')
 
+puts 'Creating blog post..'
 blog_post = user.blog_posts.create(title: 'foo', body: 'bar')
 
+puts 'Creating comment..'
 blog_post.comments.create(message: 'foobar', user: user)
